58792a478e8c7c4071f6d1d27a68efa99bf35ae3
Vilius Tamašauskas <viliustam36@gmail.com>
2025-02-16 05:26:02 +0200

    Template

---
+++ ai-elearning-system-fe/src/components/Home/Hero.jsx
@@ -0,0 +1,13 @@
+import Typography from "@mui/material/Typography";
+
+export default function Hero() {
+    return (
+        <div className="hero">
+            <div className="hero-content">
+                <Typography variant="h2" color="black">
+                    Welcome to Our E-Learning Platform
+                </Typography>
+            </div>
+        </div>
+    );
+}
