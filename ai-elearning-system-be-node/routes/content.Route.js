17d7536cbd917545260bf32c7cfaaeea790f5774
Vilius Tamašauskas <viliustam36@gmail.com>
2025-04-03 20:25:41 +0300

    Structured backend

--- ai-elearning-system-be-node/routes/content.Route.js
+++ ai-elearning-system-be-node/routes/content.Route.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const { addContent, getContent, getContentByCourseId } = require("../controllers/content.Controller");
+const {
+    addContent,
+    getContent,
+    getContentByCourseId,
+} = require("../controllers/content.Controller");
 
 const router = express.Router();
 
-// Add content to course
-router.post('/addContent', addContent);
-
-// Add content to course
+router.post("/addContent", addContent);
 router.get("/", getContent);
-
 router.get("/:courseId", getContentByCourseId);
 
 module.exports = router;
--- ai-elearning-system-be-node/routes/course.routes.js
+++ ai-elearning-system-be-node/routes/course.routes.js
@@ -1,20 +1,10 @@
 const express = require("express");
 const Course = require("../models/course.Model");
-const { saveCourse } = require("../controllers/course.Controller");
+const { getCourse, saveCourse } = require("../controllers/course.Controller");
 
 const router = express.Router();
 
-// Gauti visus kursus
-router.get("/", async (req, res) => {
-    try {
-        const courses = await Course.find();
-        res.json(courses);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
-
-// Sukurti naują kursą
+router.get("/", getCourse);
 router.post("/saveCourse", saveCourse);
 
 module.exports = router;
