2736920858513f469211d128a8fc936a18f15874
Agnė <agnlin1@ktu.lt>
2025-04-06 20:35:22 +0300

    Kurso redagavimo funkcionalumas
    Pridėtas kurso redagavimo funkcionalumas bei kurso gavimas pagal jo id.

--- ai-elearning-system-be-node/routes/course.routes.js
+++ ai-elearning-system-be-node/routes/course.routes.js
@@ -4,12 +4,17 @@ const {
     saveCourse,
     deleteCourse,
     getAllCourses,
+    updateCourse,
+    getCourseById
 } = require("../controllers/course.Controller");
 
 const router = express.Router();
 
+
 router.get("/", getAllCourses);
 router.post("/saveCourse", authenticate, authorize("Editor"), saveCourse);
 router.delete("/:id", authenticate, authorize("Editor"), deleteCourse);
+router.put("/:id", authenticate, authorize("Editor"), updateCourse);
+router.get("/:id", getCourseById);
 
 module.exports = router;
